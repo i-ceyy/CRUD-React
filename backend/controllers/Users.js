6381266033194cf53a176d6cf8a339220f9f5269
i-ceyy <iceyyworkpurposes@gmail.com>
2024-05-11 20:26:11 +0700

    API for Users
    Get all user and single user, with examples

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -1,15 +1,49 @@
+import { where } from "sequelize";
 import Users from "../models/UserModel.js";
+import argon2 from "argon2";
 
-export const getUsers = (req, res) =>{
-
+export const getUsers = async(req, res) =>{
+    try {
+        const response = await Users.findAll({
+            attributes: ['uuid', 'name', 'email', 'role']
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
 
-export const getUserById = (req, res) =>{
-    
+export const getUserById = async(req, res) =>{
+    try {
+        const response = await Users.findOne({
+            attributes: ['uuid', 'name', 'email', 'role'],
+            where: {
+                uuid: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
 
-export const createUser = (req, res) =>{
-    
+export const createUser = async(req, res) =>{
+    const {name, email, password, confPassword, role} = req.body;
+    if(password != confPassword) return res.status(400).json({msg: "Password Mismatch"});
+
+    const hashPass = await argon2.hash(password);
+
+    try {
+        await Users.create({
+            name : name,
+            email: email,
+            password: password,
+            role: role
+        });
+        res.status(201).json({msg: "User registered successfully"});
+    } catch (error) {
+        res.status(400).json({msg: error.message});
+    }
 }
 
 export const updateUser = (req, res) =>{
