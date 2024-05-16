37b32eccc36d6afb1c3549dec3cc9e56e078cf54
i-ceyy <iceyyworkpurposes@gmail.com>
2024-05-16 08:01:03 +0700

    Revert "Basic ui"
    This reverts commit e19db9f7a449fddfaf998e9f8c7b23d82238a0bf.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 function App() {
@@ -10,10 +12,71 @@ function App() {
         </Routes>
       </BrowserRouter>
 =======
+=======
+>>>>>>> parent of a2a3684 (Preparing for the frontend)
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+<<<<<<< HEAD
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+>>>>>>> parent of a2a3684 (Preparing for the frontend)
+=======
+
+function App() {
+  return (
+    <div >
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis, obcaecati?
+>>>>>>> parent of e19db9f (Basic ui)
+=======
 
 function App() {
   return (
