2ca145d3bf1b6cb848c4a7b9307da1d3b59a776c
i-ceyy <iceyyworkpurposes@gmail.com>
2024-05-18 14:18:33 +0700

    Navbar and Sidebar
    Using bulma dependencies Navbar and Sidebar was added

---
+++ frontend/src/components/Welcom.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Welcom = () => {
+  return (
+    <div>
+        <h1 className='title'> Dashboard </h1>
+        <h1 className='subtitle'> welcome back </h1>
+    </div>
+  )
+}
+
+export default Welcom
