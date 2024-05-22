1483a3ebe034dd929d1891e580e68775fab2a68b
i-ceyy <iceyyworkpurposes@gmail.com>
2024-05-22 19:50:51 +0700

    New Features
    features added:
    - CRUD User
    - CRUD product
    
    *and fixing NOT NULL attributes on the User and Products DB via the models

--- frontend/src/components/FormAddProduct.jsx
+++ frontend/src/components/FormAddProduct.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const FormAddProduct = () => {
+    const [name, setName] = useState();
+    const [price, setPrice] = useState();
+    const [msg, setMsg] = useState();
+    const navigate = useNavigate();
+
+    const saveProduct = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post("http://localhost:5000/products", {
+                name: name,
+                price: price
+            });
+            navigate("/products")
+        } catch (error) {
+            if(error.response){
+                setMsg(error.response.data.msg);
+            }
+        }
+        
+    }
   return (
     <div>
         <h1 className='title'> Product </h1>
@@ -8,22 +30,23 @@ const FormAddProduct = () => {
         <div className="card is-shadowless">
             <div className="card-content">
                 <div className="conten">
-                <form >
+                <form onSubmit={saveProduct}>
+                    <p className='has-text-centered'>{msg}</p>
                 <div className="field">
                         <label className='label'>Name</label>
                         <div className="control">
-                            <input type='text' className='input' placeholder='Product Name'></input>
+                            <input type='text' value={name} onChange={(e) => setName(e.target.value)} className='input' placeholder='Product Name'></input>
                         </div>
                     </div>
                     <div className="field">
                         <label className='label'>Price</label>
                         <div className="control">
-                            <input type='text' className='input' placeholder='Price'></input>
+                            <input type='text' value={price} onChange={(e) => setPrice(e.target.value)} className='input' placeholder='Price'></input>
                         </div>
                     </div>
                     <div className="field">
                         <div className="control">
-                            <button className='button is-success'>Save</button>
+                            <button type="submit" className='button is-success'>Save</button>
                         </div>                        
                     </div>
                 </form>
--- frontend/src/components/FormEditUser.jsx
+++ frontend/src/components/FormEditUser.jsx
@@ -1,6 +1,50 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
 const FormEditUser = () => {
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [confPassword, setConfPassword] = useState();
+    const [role, setRole] = useState();
+    const [msg, setMsg] = useState();
+    const navigate = useNavigate();
+
+    const {id} = useParams();
+
+    useEffect(()=>{
+        const getUserById = async ()=>{
+            try {
+                const response = await axios.get(`http://localhost:5000/users/${id}`);
+                setName(response.data.name);
+                setEmail(response.data.email);
+                setRole(response.data.role);
+            } catch (error) {
+                if(error.response){
+                    setMsg(error.response.data.msg);
+                }
+            }
+        }
+        getUserById();
+    }, [id]);
+
+    const updateUser = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.patch(`http://localhost:5000/users/${id}`, {
+                name: name,
+                email: email,
+                password: password,
+                confPassword: confPassword,
+                role: role
+            });
+            navigate("/users")
+        } catch (error) {
+            if(error.response){
+                setMsg(error.response.data.msg);
+            }
+        }
+    }
   return (
     <div>
         <h1 className='title'> users </h1>
@@ -8,36 +52,37 @@ const FormEditUser = () => {
         <div className="card is-shadowless">
             <div className="card-content">
                 <div className="conten">
-                <form >
+                <form onSubmit={updateUser}>
+                <p className='has-text-centered'>{msg}</p>
                 <div className="field">
                         <label className='label'>Name</label>
                         <div className="control">
-                            <input type='text' className='input' placeholder='Name'></input>
+                            <input type='text' value={name} onChange={(e) => setName(e.target.value)} className='input' placeholder='Name'></input>
                         </div>
                     </div>
                     <div className="field">
                         <label className='label'>Email</label>
                         <div className="control">
-                            <input type='text' className='input' placeholder='Email'></input>
+                            <input type='text' value={email} onChange={(e) => setEmail(e.target.value)} className='input' placeholder='Email'></input>
                         </div>
                     </div>
                     <div className="field">
                         <label className='label'>Password</label>
                         <div className="control">
-                            <input type='password' className='input' placeholder='Password'></input>
+                            <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} className='input' placeholder='Password'></input>
                         </div>
                     </div>
                     <div className="field">
                         <label className='label'>Confirm Password</label>
                         <div className="control">
-                            <input type='password' className='input' placeholder='Conf Password'></input>
+                            <input type='password' value={confPassword} onChange={(e) => setConfPassword(e.target.value)} className='input' placeholder='Conf Password'></input>
                         </div>
                     </div>
                     <div className="field">
                         <label className='label'>Role</label>
                         <div className="control">
                             <div className="select is-fullwidth">
-                                <select>
+                                <select value={role} onChange={(e) => setRole(e.target.value)}>
                                     <option value="admin">Admin</option>
                                     <option value="user">User</option>
                                 </select>
@@ -46,7 +91,7 @@ const FormEditUser = () => {
                     </div>
                     <div className="field">
                         <div className="control">
-                            <button className='button is-success'>Update</button>
+                            <button type='submit' className='button is-success'>Update</button>
                         </div>                        
                     </div>
                 </form>
